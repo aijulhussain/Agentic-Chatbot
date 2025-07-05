68cdc664f49eaca7de39d79d9c3d7ad19f493bce
aijulhussain <aijulhussain14336@gmail.com>
2025-07-05 21:03:55 +0530

    set api key

--- src/langgraphagenticai/LLMS/groqllm.py
+++ src/langgraphagenticai/LLMS/groqllm.py
@@ -14,7 +14,7 @@ class GroqLLM:
             if groq_api_key=='' and os.environ["GROQ_API_KEY"]=='':
                 st.error("Please Enter the Groq API KEY")
             
-            llm=ChatGroq(api_key==groq_api_key,model=selected_groq_model)
+            llm=ChatGroq(api_key=groq_api_key,model=selected_groq_model)
         except Exception as e:
             raise ValueError(f"Error Ocuured With Exception: {e}")
         return llm
