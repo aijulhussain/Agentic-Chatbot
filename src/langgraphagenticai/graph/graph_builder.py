654dabaf92e4a5495374e3e7daf33acb8e4db4c0
aijulhussain <aijulhussain14336@gmail.com>
2025-07-06 21:47:18 +0530

    setup for search with web

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -20,6 +20,7 @@ class GraphBuilder:
         self.basic_chatbot_node=BasicChatbotNode(self.llm)
 
         self.graph_builder.add_node("chatbot",self.basic_chatbot_node.process)
+        
         self.graph_builder.add_edge(START,"chatbot")
         self.graph_builder.add_edge("chatbot",END)
 
